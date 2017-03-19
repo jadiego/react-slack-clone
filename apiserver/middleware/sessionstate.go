703db22dbd001cc94fed9c900ea2ef1547506574
Dave Stearns <dlsinfo@uw.edu>
2017-03-19 16:01:11 -0700

    adjust comments

--- apiserver/middleware/sessionstate.go
+++ apiserver/middleware/sessionstate.go
@@ -39,7 +39,7 @@ func GetSessionState(sessionsManager *sessions.Manager) Adapter {
 
 			//get session state from the `sessionsManager`
 			//if you get an error, check what kind of error it is:
-			// - if you get sessions.ErrNoSessionToken or sessions.ErrStateNotFound
+			// - if you get sessions.ErrNoSessionID or sessions.ErrStateNotFound
 			//   then begin a new session, and keep going
 			// - if you get some other kind of error, respond with an StatusInternalServerError
 			//   sending the error message and return
@@ -55,11 +55,6 @@ func GetSessionState(sessionsManager *sessions.Manager) Adapter {
 			//after the handler returns, save the session state
 			//using sessionsManager
 
-			//for EXTRA CREDIT, do this save using a go routine so
-			//that it's done asynchronously, and does not delay the
-			//response to the client
-			//report any errors to log.Errorf()
-			//HINT: https://gobyexample.com/goroutines
 		})
 	}
 }
