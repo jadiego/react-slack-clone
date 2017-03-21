d9e2d1dba0009fbd0e27c4b11e5763858bb3857f
Dave Stearns <dlsinfo@uw.edu>
2017-03-21 10:02:21 -0700

    configurable CORS middleware with associated constants

--- apiserver/middleware/cors.go
+++ apiserver/middleware/cors.go
@@ -2,23 +2,47 @@ package middleware
 
 import "net/http"
 
+const (
+	//DefaultCORSOrigins are the default allowed origins
+	DefaultCORSOrigins = "*"
+	//DefaultCORSMethods are the default allowed methods
+	DefaultCORSMethods = "GET, PUT, POST, PATCH, DELETE"
+	//DefaultCORSAllowHeaders are the default allowed request headers
+	DefaultCORSAllowHeaders = "Content-Type, Authorization"
+	//DefaultCORSExposeHeaders are the default exposed response headers
+	DefaultCORSExposeHeaders = "Authorization"
+)
+
+//constants for CORS header names
+const (
+	headerAccessControlAllowOrigin   = "Access-Control-Allow-Origin"
+	headerAccessControlAllowHeaders  = "Access-Control-Allow-Headers"
+	headerAccessControlExposeHeaders = "Access-Control-Expose-Headers"
+	headerAccessControlAllowMethods  = "Access-Control-Allow-Methods"
+)
+
 //CORS is a middleware function that adds the CORS headers to the response
 //so that clients from different origins can call our APIs
-func CORS() Adapter {
-	//return an Adapter function...
-	return func(handler http.Handler) http.Handler {
-		//that returns an http.Handler that...
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//does the following:
-			// - adds the following response headers to every request:
-			//    - Access-Control-Allow-Origin: *
-			//    - Access-Control-Allow-Headers: Content-Type, X-Session-ID
-			//    - Access-Control-Allow-Methods: GET, PUT, POST, PATCH, DELETE
-			//    - Access-Control-Expose-Headers: X-Session-ID
+func CORS(origins, methods, allowHeaders, exposeHeaders string) Adapter {
+	//if the origins, methods, allowHeaders, and exposeHeaders
+	//parameters are zero-length, default them to their default
+	//constant values listed above
 
-			// - if the request method is OPTIONS (pre-flight CORS request),
-			//   simply responds with http.StatusOK
-			//   else, calls the handler's ServeHTTP() method
+	//return an Adapter function that...
+	return func(handler http.Handler) http.Handler {
+		//returns an http.Handler that...
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//add the following response headers to every request:
+			// - Access-Control-Allow-Origin: value of origins param
+			// - Access-Control-Allow-Methods: value of methods param
+			// - Access-Control-Allow-Headers: value of allowHeaders param
+			// - Access-Control-Expose-Headers: value of exposeHeaders param
+
+			//if the request method is OPTIONS, this is a pre-flight
+			//CORS request to see if the real request should be allowed
+			//so simply respond with no body and http.StatusOK
+
+			//else, call the ServeHTTP() method on `handler`
 
 		})
 	}
