fe9439687879ba255c851e28fbb377739c1deddd
John Diego <jadiego@uw.edu>
2017-04-02 21:09:18 -0700

    add summary handler

--- apiserver/main.go
+++ apiserver/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/info344-s17/challenges-jadiego/apiserver/handlers"
+)
+
 const defaultPort = "80"
 
 const (
@@ -7,23 +16,32 @@ const (
 	apiSummary = apiRoot + "summary"
 )
 
-//TODO: implement the main() function as directed by the comments.
 //main is the main entry point for this program
 func main() {
 	//read and use the following environment variables
 	//when initializing and starting your web server
 	// PORT - port number to listen on for HTTP requests (if not set, use defaultPort)
 	// HOST - host address to respond to (if not set, leave empty, which means any host)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		fmt.Println("Port number not set. Defaulting to port: " + defaultPort)
+		port = defaultPort
+	}
+	host := os.Getenv("HOST")
+	if len(host) == 0 {
+		fmt.Println("Host not set. Defaulting to empty host")
+		host = ""
+	}
 
 	//add your handlers.SummaryHandler function as a handler
 	//for the apiSummary route
 	//HINT: https://golang.org/pkg/net/http/#HandleFunc
+	http.HandleFunc(apiSummary, handlers.SummaryHandler)
 
 	//start your web server and use log.Fatal() to log
 	//any errors that occur if the server can't start
 	//HINT: https://golang.org/pkg/net/http/#ListenAndServe
+	fmt.Printf("server is listening at %s:%s...\n", host, port)
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
 
 }
-
-// NEW line for PR
-// line 2
