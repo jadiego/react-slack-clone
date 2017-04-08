07909a904c5ed3f24046a30c681d5faf6a2a3652
John Diego <jadiego@uw.edu>
2017-04-07 22:42:18 -0700

    changed bootstrap to semantics ui

--- webclient/src/components/App.js
+++ webclient/src/components/App.js
@@ -1,80 +1,144 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Menu, Input, Container, Message, Card } from 'semantic-ui-react'
 
-import { Navbar, Nav, NavItem, Form, FormControl, InputGroup, Button, Glyphicon } from 'react-bootstrap';
-
-import '../css/App.css';
 
 class TopNavbar extends Component {
 	render() {
 		return (
-			<Navbar fixedTop={true}>
-				<Navbar.Header>
-					<Navbar.Brand>
-						<a href="#">OGPSearch</a>
-					</Navbar.Brand>
-					<Navbar.Toggle />
-				</Navbar.Header>
-				<Navbar.Collapse>
-					<Nav pullRight>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
+			<Menu fixed='top'>
+				<Container>
+					<Menu.Item header>
+						OGPSearch
+						</Menu.Item>
+				</Container>
+			</Menu>
 		);
 	}
 }
 
 
-class ogpcard extends Component {
+class OGPCard extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		if (!this.props.show) {
+			return null
+		} else {
+			return (
+				<Card
+					image={this.props.img}
+					header={this.props.title}
+					description={this.props.description}
+					href={this.props.url}
+					>
+
+				</Card>
+			)
+		}
+	}
+}
+
+class InvalidURLWarningMessage extends Component {
+	constructor(props) {
+		super(props)
+	}
+
 	render() {
 		return (
-			<div></div>
+			<Message warning>
+				<Message.Header>You must use a valid URL!</Message.Header>
+				<p>{this.props.errmessage}</p>
+			</Message>
 		)
 	}
 }
 
 class SearchForm extends Component {
 
+
+	constructor(props) {
+		super(props);
+
+		// This binding is necessary to make `this` work in the callback
+		this.handleSearch = this.handleSearch.bind(this);
+	}
+
 	// method to call when search button is clicked
-	search = (e) => {
+	handleSearch = (e) => {
 		e.preventDefault();
-		let url = document.querySelector("#url").value;
+		let url = document.querySelector("#url input").value;
 		console.log("Searching:", url)
 		// call on API to get Opengraph properties
-		fetch("http://localhost:8000/v1/summary?url=" + url)
+		fetch("http://104.131.146.195/v1/summary?url=" + url)
 			.then(function (resp) {
 				return resp.json()
 			})
-			.then(function(data) {
-				console.log(data)
+			.then(function (data) {
+				console.log("Results :", data)
+				let warningcontainer = document.querySelector("#results-list");
+				warningcontainer.innerHTML = "";
 				// When the server responds with a JSON of the OGP properties
 				// render the summary data to the page. Atleast renders the
 				// image, title, and description property.
 				// Handles missing properties gracefully.
 				// Communicates back the error if it comes across one.
-
+				
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				console.log(err)
+				let warningcontainer = document.querySelector("#results-list");
+				warningcontainer.innerHTML = "";
+				let warningmsg = document.createElement("div")
+				warningmsg.className = "warningmsg"
+				let a = document.createElement("strong");
+				a.innerText = "Woops! Try another link!"
+				let b = document.createElement("p")
+				b.innerText = err;
+				warningmsg.appendChild(a);
+				warningmsg.appendChild(b);
+				warningcontainer.appendChild(warningmsg);
 			})
 	}
 
 	render() {
 		return (
-			<Form id="search-form" onSubmit={this.search}>
-				<p>Search for a web page <strong>URL</strong> here! It will bring back a <strong>summary</strong> of the webpage.</p>
-				<InputGroup bsSize="small">
-					<InputGroup.Button onClick={this.search}>
-						<Button>
-							<Glyphicon glyph="search" aria-label="Search" />
-						</Button>
-					</InputGroup.Button>
-					<FormControl id="url" name="url" type="text" placeholder="Search..." />
-					<InputGroup.Addon onClick={this.search}> Search </InputGroup.Addon>
-				</InputGroup>
-			</Form>
+			<div id='search-input'>
+				<p>Search for a <strong>web page</strong> here! It will bring back a <strong>summary</strong> of the webpage.</p>
+
+				<Input id='url' as='form' onSubmit={this.handleSearch} fluid size='tiny' action={{ icon: 'search', id: "searchbtn", onClick: this.handleSearch }} placeholder='http://ogp.me/' />
+			</div>
 		);
 	}
 }
+
+class OGPCardControl extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { gotResults: false }
+	}
+
+	render() {
+		const didGetResults = this.state.gotResults;
+
+		if (didGetResults) {
+			return (
+				<Card
+					image={this.props.img}
+					header={this.props.title}
+					description={this.props.description}
+					href={this.props.url}
+					>
+
+				</Card>
+			)
+		} else {
+			return null
+		}
+	}
+}
+
 
 class App extends Component {
 	render() {
@@ -84,10 +148,11 @@ class App extends Component {
 				<section id="search-container">
 					<SearchForm></SearchForm>
 				</section>
-				<section id="results-container" fluid={true}>
+				<section id="results-container" >
 					<h2 >Results</h2>
 					<hr id="break" />
-					<div id="results-list" fluid={true}>
+					<div id="results-list" >
+						<OGPCardControl />
 					</div>
 				</section>
 			</div>
--- webclient/src/index.js
+++ webclient/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import 'bootstrap/dist/css/bootstrap.css';
+
+import 'semantic-ui-css/semantic.min.css';
 import './css/index.css';
 
 ReactDOM.render(
