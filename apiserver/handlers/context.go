ad135d7b15cf9344e513d1e4bf2938ef3ac86ec6
John Diego <jadiego@uw.edu>
2017-04-21 22:37:48 -0700

    implement context

---
+++ apiserver/handlers/context.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.com/info344-s17/challenges-jadiego/apiserver/models/users"
+	"github.com/info344-s17/challenges-jadiego/apiserver/sessions"
+)
+
+//Context is a way for the main function to provide
+//dependencies to the handlers as it adds them to the mux.
+type Context struct {
+	//a string that will contain the session ID signing key
+	SessionKey string
+	//a sessions.Store that will contain the server's session store
+	SessionStore sessions.Store
+	//an users.Store that will contain the server's user store
+	UserStore users.Store
+}
