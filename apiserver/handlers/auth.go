94718e93f1f55b742321ac1bdac2367fc6bd4162
John Diego <jadiego@uw.edu>
2017-05-12 22:24:37 -0700

    update UsersMeHandler to handler error when no current session

--- apiserver/handlers/auth.go
+++ apiserver/handlers/auth.go
@@ -149,17 +149,22 @@ func (ctx *Context) SessionsMineHandler(w http.ResponseWriter, r *http.Request) 
 
 //UsersMeHanlder gets the session state
 func (ctx *Context) UsersMeHanlder(w http.ResponseWriter, r *http.Request) {
+	//get the current user to get context for which update this belongs to
+	ss := &SessionState{}
+	_, err := sessions.GetState(r, ctx.SessionKey, ctx.SessionStore, ss)
+	if err != nil {
+		http.Error(w, "error getting current state : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
 	switch r.Method {
 	case "GET":
-		//get the session state
-		ss := &SessionState{}
-		sessions.GetState(r, ctx.SessionKey, ctx.SessionStore, ss)
-
 		//Respond to the client with the session
 		//state's User field, encoded as a JSON object
 		w.Header().Add(headerContentType, contentTypeJSONUTF8)
 		encoder := json.NewEncoder(w)
 		encoder.Encode(ss.User)
+
 	case "PATCH":
 		//read and decode whatever gets posted
 		//decode whats being passed into a userupdates model
@@ -168,14 +173,6 @@ func (ctx *Context) UsersMeHanlder(w http.ResponseWriter, r *http.Request) {
 		userupdate := &users.UserUpdates{}
 		if err := decoder.Decode(userupdate); err != nil {
 			http.Error(w, "error decoding JSON: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		//get the current user to get context for which update this belongs to
-		ss := &SessionState{}
-		_, err := sessions.GetState(r, ctx.SessionKey, ctx.SessionStore, ss)
-		if err != nil {
-			http.Error(w, "error getting current state : "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
