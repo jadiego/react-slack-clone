2903219a77e7d24d2b0bd8013918048ada802cec
John Diego <jadiego@uw.edu>
2017-05-12 22:18:27 -0700

    create mongostore and test

--- apiserver/models/messages/mongostore.go
+++ apiserver/models/messages/mongostore.go
@@ -1,7 +1,7 @@
 package messages
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/info344-s17/challenges-jadiego/apiserver/models/users"
 	"gopkg.in/mgo.v2"
@@ -17,19 +17,38 @@ type MongoStore struct {
 }
 
 //GetMyChannels get all channels a given user is allowed to see (i.e.,
-//channels the user is a member of, as well as all public channels)
+//channels the user is a member of, as well as all public channels, if is creator)
 func (ms *MongoStore) GetMyChannels(id users.UserID) ([]*Channel, error) {
 	channels := []*Channel{}
-	query := bson.M{"members": id, "private": false}
+	idquery := bson.M{"members": id}
+	privatequery := bson.M{"private": false}
+	query := bson.M{"$or": []bson.M{idquery, privatequery}}
 	err := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName).Find(query).All(&channels)
 	return channels, err
 }
 
+//GetChannelByID gets a single channel by the given id parameter
+func (ms *MongoStore) GetChannelByID(id ChannelID) (*Channel, error) {
+	channel := &Channel{}
+	err := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName).FindId(id).One(channel)
+	if err == mgo.ErrNotFound {
+		return nil, ErrChannelNotFound
+	}
+	return channel, err
+}
+
 //InsertChannel inserts a new channel into the store
 func (ms *MongoStore) InsertChannel(id users.UserID, newchannel *NewChannel) (*Channel, error) {
+	if err := newchannel.Validate(); err != nil {
+		return nil, err
+	}
 	c := newchannel.ToChannel()
+
+	//attach creator's id in new channel
 	c.CreatorID = id
+	//create id of channel
 	c.ID = ChannelID(bson.NewObjectId().Hex())
+
 	err := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName).Insert(c)
 	return c, err
 }
@@ -44,11 +63,18 @@ func (ms *MongoStore) GetChannelMessages(id ChannelID, n int) ([]*Message, error
 }
 
 //UpdateChannel updates a channels name and description
-func (ms *MongoStore) UpdateChannel(updates *ChannelUpdates, currentchannel *Channel) error {
+func (ms *MongoStore) UpdateChannel(updates *ChannelUpdates, currentchannel *Channel) (*Channel, error) {
 	col := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName)
 	channelupdates := bson.M{"$set": updates}
 	err := col.UpdateId(currentchannel.ID, channelupdates)
-	return err
+	if err != nil {
+		return nil, err
+	}
+	updatedchannel := currentchannel
+	updatedchannel.Name = updates.Name
+	updatedchannel.Description = updates.Description
+
+	return updatedchannel, nil
 }
 
 //DeleteChannel delete a channel, as well as all messages posted to that channel
@@ -66,47 +92,80 @@ func (ms *MongoStore) DeleteChannel(id ChannelID) error {
 }
 
 //AddChannelMember add a user to a channel's Members list
-func (ms *MongoStore) AddChannelMember(member *users.User, currentchannel *Channel) error {
-	//returns error if member is already a member of channel
-	for _, v := range currentchannel.Members {
-		if v == member.ID {
-			return fmt.Errorf("user is already a member")
-		}
-	}
-
-	membersupdates := bson.M{"$set": append(currentchannel.Members, member.ID)}
-	err := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName).UpdateId(currentchannel.ID, membersupdates)
+func (ms *MongoStore) AddChannelMember(id users.UserID, currentchannel *Channel) error {
+	col := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName)
+	memberupdates := bson.M{"$addToSet": bson.M{"members": id}}
+	err := col.UpdateId(currentchannel.ID, memberupdates)
 	return err
 }
 
-//FIX WRONG IMPLEMENTATION
 //RemoveChannelmember remove a user from a channel's Members list
-func (ms *MongoStore) RemoveChannelmember(member *users.User, currentchannel *Channel) error {
-	//returns error if member is already not a member of channel
-	foundmember := false
-	index := 0
-	for i, v := range currentchannel.Members {
-		if v == member.ID {
-			foundmember = true
-			index = i
-			break
-		}
-	}
-	if !foundmember {
-		return fmt.Errorf("user is already not a member")
-	}
-
-	membersupdates := bson.M{"$set": append(currentchannel.Members, member.ID)}
-	err := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName).UpdateId(currentchannel.ID, membersupdates)
+func (ms *MongoStore) RemoveChannelmember(id users.UserID, currentchannel *Channel) error {
+	col := ms.Session.DB(ms.DatabaseName).C(ms.ChannelsCollectionName)
+	memberupdates := bson.M{"$pull": bson.M{"members": id}}
+	err := col.UpdateId(currentchannel.ID, memberupdates)
 	return err
 }
 
-// //InsertMessage inserts a new message and returns a new Message
-// //with a newly assigned ID
-// InsertMessage(id users.UserID, newmessage *NewMessage) (*Message, error)
+//InsertMessage inserts a new message and returns a new Message
+//with a newly assigned ID
+func (ms *MongoStore) InsertMessage(id users.UserID, newmessage *NewMessage) (*Message, error) {
+	m := newmessage.ToMessage()
+	m.CreatorID = id
+	m.ID = MessageID(bson.NewObjectId().Hex())
+	err := ms.Session.DB(ms.DatabaseName).C(ms.MessagesCollectionName).Insert(m)
+	return m, err
+}
 
-// //UpdateMessage applies the MessageUpdates to the current message
-// UpdateMessage(updates *MessageUpdates, currentmessage *Message) error
+//UpdateMessage applies the MessageUpdates to the current message
+func (ms *MongoStore) UpdateMessage(updates *MessageUpdates, currentmessage *Message) (*Message, error) {
+	currentmessage.EditedAt = time.Now()
+	col := ms.Session.DB(ms.DatabaseName).C(ms.MessagesCollectionName)
+	messageupdates := bson.M{"$set": updates}
+	err := col.UpdateId(currentmessage.ID, messageupdates)
+	if err != nil {
+		return nil, err
+	}
+	updatedmessage := currentmessage
+	updatedmessage.Body = updates.Body
 
-// //DeleteMessage deletes a message
-// DeleteMessage(id MessageID) error
+	return updatedmessage, nil
+}
+
+//DeleteMessage deletes a message
+func (ms *MongoStore) DeleteMessage(id MessageID) error {
+	err := ms.Session.DB(ms.DatabaseName).C(ms.MessagesCollectionName).RemoveId(id)
+	return err
+}
+
+//NewMongoStore constructs a new MongoStore using the provided
+//addr. If the 'mongoAddr' is nil, it will use a default port and host
+func NewMongoStore(mongoAddr, DBName, ChannelCollectionName, MessageCollectionName string) (*MongoStore, error) {
+	if len(mongoAddr) == 0 {
+		mongoAddr = "localhost:27017"
+	}
+
+	sess, err := mgo.Dial(mongoAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(DBName) == 0 {
+		DBName = "chat"
+	}
+
+	if len(ChannelCollectionName) == 0 {
+		ChannelCollectionName = "channels"
+	}
+
+	if len(MessageCollectionName) == 0 {
+		MessageCollectionName = "messages"
+	}
+
+	return &MongoStore{
+		Session:                sess,
+		DatabaseName:           DBName,
+		ChannelsCollectionName: ChannelCollectionName,
+		MessagesCollectionName: MessageCollectionName,
+	}, nil
+}
