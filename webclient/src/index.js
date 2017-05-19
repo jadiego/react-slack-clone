cae69d5352ca1fce6b330ca4085ce3c0082cd519
John Diego <jadiego@uw.edu>
2017-05-18 22:00:58 -0700

    include redux into app

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import 'whatwg-fetch';
+
 
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import rootReducer from './reducers';
+
+//store setup for redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
-        <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
