4816b160318482f2bde99a4286309c70fd9a190c
John Diego <jadiego@uw.edu>
2017-06-04 23:47:07 -0700

    add switch case for intent

--- chatbot/handlers/messages.js
+++ chatbot/handlers/messages.js
@@ -2,29 +2,42 @@
 
 const express = require('express');
 const { Wit } = require("node-wit");
-const witaiToken = process.env.WITAITOKEN;
+const handle = require("./intent.js")
 
+//setup wit ai
+const witaiToken = process.env.WITAITOKEN;
 if (!witaiToken) {
   console.error("please set WITAITTOKEN to your wit.ai app token");
   process.exit(1);
 }
-
 const witaiClient = new Wit({ accessToken: witaiToken });
 
-
-module.exports = function(store) {
+module.exports = function (store) {
   let router = express.Router();
-  
+
   router.post('/v1/bot', (req, res, next) => {
     try {
       let q = req.query.q;
-      console.log(`user is asking ${q}`);
+      let user = JSON.parse(req.header('X-User'));
+
+      console.log(`user ${user.email} is asking ${q}`);
+
       witaiClient.message(q)
         .then(data => {
-          res.send(JSON.stringify(data, undefined, 2))
+          //console.log(JSON.stringify(data, undefined, 2));
+          switch (data.entities.intent[0].value) {
+            case "late-post":
+              handle.lateMessage(res, data, store, user);
+              break;
+            case "count":
+              handle.countMessages(res, data, store, user);
+              break;
+            default:
+              res.send("Sorry, I'm not sure how to answer that. Please try again.");
+          }
         })
-        .catch(next)
-    } catch(err) {
+        .catch(next);
+    } catch (err) {
       next(err);
     }
   })
