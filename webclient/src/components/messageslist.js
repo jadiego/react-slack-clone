1e78be2a69f9ce00c3d435258b3b449236d30607
John Diego <jadiego@uw.edu>
2017-06-14 10:06:32 -0700

    fixed dates on comments

--- webclient/src/components/messageslist.js
+++ webclient/src/components/messageslist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Container, Item, Image, Breadcrumb } from 'semantic-ui-react';
+import { Segment, Container, Item, Image, Breadcrumb, Divider } from 'semantic-ui-react';
 import { find } from 'lodash';
 import moment from "moment";
 
@@ -15,50 +15,46 @@ class MessagesList extends Component {
   render() {
     const { messages, currentChannel, users } = this.props;
     const myMessages = messages[currentChannel.id];
-    let prevDate = {};
-    let prevUser = '';
-    let group = 0;
-    // if (myMessages !== undefined) {
-    //   myMessages.map((m, i, a) => {
-    //     if (m.creatorid !== prevUser) {
-    //       prevUser = m.creatorid;
-    //       group++;
-    //     }
-    //     m.group = group;
-    //     if (a[i - 1] !== undefined) {
-    //       if (moment(m.createdAt).diff(moment(a[i - 1].createdAt), 'minutes') > 10 ||
-    //         moment(m.createdAt).diff(moment(a[i - 1].createdAt), 'days') >= 1) {
-    //         group++
-    //         m.group = group;
-    //       }
-    //     }
-    //     return m;
-    //   });
-    // }
+
     return (
       <Container fluid className='items'>
         {myMessages !== undefined && (myMessages.map((m, i, a) => {
           let u = find(users, u => u.id === m.creatorid);
-          return <Item key={m.id}>
+          return <Item.Group style={{margin:0}}>
             {(a[i - 1] !== undefined) ? (
-              differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'days', 1) && <div>{moment(m.createdAt).format('LL')}</div>
+              (moment(m.createdAt).dayOfYear() !== moment(a[i - 1].createdAt).dayOfYear()) && <Divider horizontal className='comment-date'>{moment(m.createdAt).format('LL')}</Divider>
             ) : (
-                <div>{moment(m.createdAt).format('LL')}</div>
+                <Divider horizontal className='comment-date'>{moment(m.createdAt).format('LL')}</Divider>
               )}
-            {(a[i - 1] !== undefined) ? (
-              (m.creatorid !== a[i - 1].creatorid
-                || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'minutes', 8)
-                || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'days', 1)) ? (
+            <Item key={m.id}>
+              {(a[i - 1] !== undefined) ? (
+                (m.creatorid !== a[i - 1].creatorid
+                  || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'minutes', 8)) ? (
+                    <Item.Image size='mini' src={u.photoURL} />
+                  ) : (
+                    <div className='ui mini comment-time image'>{moment(m.createdAt).format('h:mm')}</div>
+                  )) : (
                   <Item.Image size='mini' src={u.photoURL} />
-                ) : (
-                  <div className='ui mini image'></div>
-                )) : (
-                <Item.Image size='mini' src={u.photoURL} />
-              )}
-            {(a[i - 1] !== undefined) ? (
-              (m.creatorid !== a[i - 1].creatorid
-                || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'minutes', 8)
-                || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'days', 1)) ? (
+                )}
+              {(a[i - 1] !== undefined) ? (
+                (m.creatorid !== a[i - 1].creatorid
+                  || differenceBetweenTwoDates(m.createdAt, a[i - 1].createdAt, 'minutes', 8)) ? (
+                    <Item.Content>
+                      <Item.Header className='comment-username'>
+                        <Breadcrumb>
+                          <Breadcrumb.Section>{u.userName}</Breadcrumb.Section>
+                          <Breadcrumb.Divider />
+                          <Breadcrumb.Section>{moment(m.createdAt).format('LT')}</Breadcrumb.Section>
+                        </Breadcrumb>
+                      </Item.Header>
+                      <Item.Description>{m.body}</Item.Description>
+                    </Item.Content>
+                  ) : (
+                    <Item.Content>
+                      <Item.Description>{m.body}</Item.Description>
+                    </Item.Content>
+                  )
+              ) : (
                   <Item.Content>
                     <Item.Header className='comment-username'>
                       <Breadcrumb>
@@ -69,24 +65,9 @@ class MessagesList extends Component {
                     </Item.Header>
                     <Item.Description>{m.body}</Item.Description>
                   </Item.Content>
-                ) : (
-                  <Item.Content>
-                    <Item.Description>{m.body}</Item.Description>
-                  </Item.Content>
-                )
-            ) : (
-                <Item.Content>
-                  <Item.Header className='comment-username'>
-                    <Breadcrumb>
-                      <Breadcrumb.Section>{u.userName}</Breadcrumb.Section>
-                      <Breadcrumb.Divider />
-                      <Breadcrumb.Section>{moment(m.createdAt).format('LT')}</Breadcrumb.Section>
-                    </Breadcrumb>
-                  </Item.Header>
-                  <Item.Description>{m.body}</Item.Description>
-                </Item.Content>
-              )}
-          </Item>
+                )}
+            </Item>
+          </Item.Group>
         }))}
       </Container>
     );
