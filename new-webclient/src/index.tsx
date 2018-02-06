4db9d25181f7598934d1cbad2040c5f0c1adf992
John Diego <jadiego@uw.edu>
2018-02-05 19:10:35 -0800

    starter files

---
+++ new-webclient/src/index.tsx
@@ -0,0 +1,21 @@
+// lib imports
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'normalize.css';
+// app imports
+import './styles/app.css';
+import landing from './components/landing';
+import notFound from './components/notFound';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact={true} path="/" component={landing} />
+            <Route path="*" component={notFound} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
