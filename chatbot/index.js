c877fc0ed33c27471f6e78f88b0b2adb9914c53d
John Diego <jadiego@uw.edu>
2018-06-04 03:42:37 -0700

    fix mongo to retry connections

--- chatbot/index.js
+++ chatbot/index.js
@@ -6,11 +6,11 @@ const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
 const cors = require("cors");
 
-const MessageStore = require('./models/messages/mongostore.js');
+const MessageStore = require("./models/messages/mongostore.js");
 
 const host = process.env.HOST || "";
 const port = process.env.PORT || "80";
-const mongoAddr = process.env.MONGOADDR || 'localhost:27017';
+const mongoAddr = process.env.MONGOADDR || "localhost:27017";
 
 const app = express();
 
@@ -24,15 +24,20 @@ app.use(bodyParser.json());
 //add cors headers
 app.use(cors());
 
+let mongoconf = {
+  reconnectTries: Number.MAX_VALUE,
+  autoReconnect: true,
+  reconnectInterval: 1000
+};
 
-mongodb.MongoClient.connect(`mongodb://${mongoAddr}/chat`)
+mongodb.MongoClient.connect(`mongodb://${mongoAddr}/chat`, mongoconf)
   .then(db => {
-    let colChannels = db.collection('channels');
-    let colMessages = db.collection('messages');
-    let colUsers = db.collection('users');
+    let colChannels = db.collection("channels");
+    let colMessages = db.collection("messages");
+    let colUsers = db.collection("users");
     let store = new MessageStore(colChannels, colMessages, colUsers);
-    let handlers = require('./handlers/messages.js');
-    app.use(handlers(store))
+    let handlers = require("./handlers/messages.js");
+    app.use(handlers(store));
 
     //error handler
     app.use((err, req, res, next) => {
@@ -41,9 +46,9 @@ mongodb.MongoClient.connect(`mongodb://${mongoAddr}/chat`)
     });
 
     app.listen(port, host, () => {
-      console.log(`server is listening at http://${host}:${port}...`)
+      console.log(`server is listening at http://${host}:${port}...`);
     });
   })
   .catch(err => {
     console.error(err);
-  })
+  });
